2c780482107043d4d19d66f2b03f600816e91c5f
fenix lam <fenixheat@gmail.com>
2012-09-28 11:44:50 +0800

    version 0.1.

---
+++ test.js
@@ -0,0 +1,11 @@
+function LV1_readASS(filename){
+	$.get(filename, function(data){
+		var subtitleInfo=readVars(data);
+		//var subtitleStyles=readStyle(data);
+		//var subtitleEvents(data);
+		
+	});
+}
+function readVars(data){
+	
+}
